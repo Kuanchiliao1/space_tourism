54bc3a436df899624dd2a401c3b50c2f0b0f4788
kuanchiliao1 <kuanchiliao@gmail.com>
2022-12-12 17:57:30 -0500

    Add mobile hamburger menu

---
+++ index.js
@@ -0,0 +1,21 @@
+const hamburgerMenu = document.querySelector(".primary-navigation")
+const navToggle = document.querySelector(".mobile-nav-toggle")
+
+navToggle.addEventListener("click", function() {
+  if (hamburgerMenu.getAttribute("data-visible") == "false") {
+    // close it
+    hamburgerMenu.setAttribute("data-visible", "true")
+    hamburgerMenu.setAttribute("aria-expanded", "true")
+    
+    // Instead of using the style methods, Kevin created new rulesets and used those to toggle the classes on and off. That method is probably more versatile than what I'm doing here.
+    hamburgerMenu.style.transform = "none";
+    navToggle.style.backgroundImage = `url("assets/shared/icon-close.svg")`
+  } else {
+    hamburgerMenu.setAttribute("data-visible", "false")
+    hamburgerMenu.setAttribute("aria-expanded", "false")
+
+    hamburgerMenu.style.transform = "translateX(100%)";
+    navToggle.style.backgroundImage = `url("assets/shared/icon-hamburger.svg")`;
+  }
+});
+
